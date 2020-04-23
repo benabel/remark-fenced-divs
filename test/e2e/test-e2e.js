aa2299a9e38e18bd44e5ab5a08c5160499e03148
Benjamin Abel <dev.abel@free.fr>
2020-04-23 15:49:06 +0200

    TEST Run all e2e tests from node

--- test/e2e/test-e2e.js
+++ test/e2e/test-e2e.js
@@ -1,10 +1,48 @@
+/**
+ * test-e2e module.
+ *
+ * Compare output of pandoc and remark for the extension fenced_fivs
+ *
+ * TODO: compare trees
+ *
+ * @module test-e2e
+ */
+
+const {spawn} = require('child_process')
+process.chdir(__dirname)
+
+const fs = require('fs')
 const unified = require('unified')
 const parse = require('remark-parse')
 const remark2rehype = require('remark-rehype')
 const stringify = require('rehype-stringify')
 const fencedDiv = require('../..')
-const fs = require('fs')
+var vfile = require('to-vfile')
+var report = require('vfile-reporter')
+var rehype = require('rehype')
+var format = require('rehype-format')
 
+//
+// convert md to html
+// with pandoc
+
+const arguments =
+  '-f markdown-auto_identifiers-smart test.md -o test-pandoc.html'
+const childPandoc = spawn('pandoc', arguments.split(' '))
+
+childPandoc.on('exit', function (code, signal) {
+  console.log('pandoc exited with ' + `code ${code} and signal ${signal}`)
+})
+
+childPandoc.stdout.on('data', (data) => {
+  console.log(`pandoc stdout:\n${data}`)
+})
+
+childPandoc.stderr.on('data', (data) => {
+  console.error(`pandoc stderr:\n${data}`)
+})
+
+// With remark using fenced_divs
 unified()
   .use(parse, {commonmark: true})
   .use(fencedDiv)
@@ -12,7 +50,21 @@ unified()
   .use(stringify, {
     quoteSmart: false
   })
-  .process(fs.readFileSync('test.md'), function (err, file) {
-    if (err) throw err
+  .process(vfile.readSync('test.md'), function (err, file) {
+    console.error(report(err || file))
     fs.writeFileSync('test-remark.html', String(file))
   })
+
+// Format files for visual comparison
+function format(htmlFile) {
+  rehype()
+    .use(format)
+    .process(vfile.readSync(htmlFile), function (err, file) {
+      console.error(report(err || file))
+      fs.writeFileSync(htmlFile, String(file))
+    })
+}
+;['test-pandoc.html', 'test-remark.html'].forEach((file) => {
+  console.log('Formatting ' + file)
+  format(file)
+})
